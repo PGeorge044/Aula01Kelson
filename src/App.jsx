d1a116de3d44cc369660d8f377acf3c375b8db38
PGeorge044 <pg8201957@gmail.com>
2025-09-02 20:04:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Adicao from './components/Adicao'
+import Subtracao from './components/Subtracao'
+import Multiplicacao from './components/multiplicacao'
+import Divisao from './components/divisao'
 
 const App = () => {
   return (
     <div>
-      <Adicao num1={2} num2={2}/>
+      <h1>Operações Matemáticas</h1>
+      
+      <Adicao num1={2} num2={2} />
+      <Subtracao num1={2} num2={2} />
+      <Multiplicacao num1={2} num2={2} />
+      <Divisao num1={2} num2={2} />
     </div>
   )
 }
 
-export default App
+export default App 
